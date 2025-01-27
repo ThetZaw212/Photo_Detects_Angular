903c9ba1ad513fa369ba8fd7ef2f57950d9fe8b0
Thet Zaw <kkothet212@gmail.com>
2025-01-27 10:51:46 +0630

    Update app.routes.ts

--- photo_detect/src/app/app.routes.ts
+++ photo_detect/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FaceRecognitionComponent } from './pages/face-recognition/face-recognition.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   
+];
